0d14154fddfb0710a9ead5d9495ad6348917a8ed
ju <ju@Mikhails-MBP.cust.communityfibre.co.uk>
2023-08-02 20:20:02 +0100

    Sprint_6 rev.2

--- src/test/java/com/example/LionConstructorExceptionTest.java
+++ src/test/java/com/example/LionConstructorExceptionTest.java
@@ -1,20 +1,20 @@
 package com.example;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThrows;
 
 public class LionConstructorExceptionTest {
-
-    @Rule
-    public ExpectedException expectedEx = ExpectedException.none();
     @Test
-    public void lionConstructorNotCorrectSexTest() throws Exception {
-        expectedEx.expect(Exception.class);
-        expectedEx.expectMessage("Используйте допустимые значения пола животного - самей или самка");
-
+    public void lionConstructorNotCorrectSexTest() {
         Feline feline = new Feline();
         String notCorrectSex = "NotCorrectLion";
-        new Lion(feline, notCorrectSex);
+        Exception exception = assertThrows(
+                Exception.class, () -> new Lion(feline, notCorrectSex));
+
+        String expectedMessage = "Используйте допустимые значения пола животного - самей или самка";
+        String message = exception.getMessage();
+        assertThat(message, is(expectedMessage));
     }
 }
